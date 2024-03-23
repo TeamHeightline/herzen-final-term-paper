518282c2cf66387a035acbd619c481e54ae6ee0e
timofei <teamheightline@mail.ru>
2024-03-23 21:32:59 +0300

    Микро изменения UI

--- app/components/message-input.tsx
+++ app/components/message-input.tsx
@@ -18,16 +18,16 @@ export default function MessageInput({onSendMessage}: MessageInputProps) {
     }
 
     return (
-        <form className={"flex flex-row w-full max-w-3xl mt-4"} onSubmit={onSubmit}>
-                <textarea rows={1} placeholder="Ваше сообщение"
+        <form className="flex flex-row w-full max-w-3xl mt-4" onSubmit={onSubmit}>
+                <textarea rows={1}
+                          placeholder="Ваше сообщение"
                           value={messageText}
                           onChange={(event) => setMessageText(event.target.value)}
-                          className={"p-4 bg-gray-100 rounded-2xl w-full border resize-none " +
-                              "focus:outline-none focus:ring-0 focus:border-gray-300"}>
+                          className="p-4 bg-gray-100 rounded-2xl w-full border resize-none focus:outline-none focus:ring-0 focus:border-gray-300">
                 </textarea>
 
             <button type="submit"
-                    className={"mx-4 p-4 bg-yellow-300 text-yandex-black rounded-2xl whitespace-nowrap transition ease-in-out hover:brightness-95 active:brightness-90"}>
+                    className="mx-4 p-4 bg-yellow-300 text-yandex-black rounded-2xl whitespace-nowrap transition ease-in-out hover:brightness-95 active:brightness-90">
                 Отправить сообщение
             </button>
         </form>
--- app/components/messages.tsx
+++ app/components/messages.tsx
@@ -7,7 +7,7 @@ interface MessageProps {
 
 // Стиль для сообщений текущего пользователя
 const currentUserStyle = "mr-auto text-white bg-gradient-to-tr from-red-500 to-pink-500";
-// Стиль для сообщений собеседника
+// Стиль для сообщений от ИИ
 const otherUserStyle = "ml-auto text-black bg-yandex-blue";
 
 export default function Messages({messages}: MessageProps) {
@@ -15,7 +15,7 @@ export default function Messages({messages}: MessageProps) {
         <>
             {messages.map((message, index) => (
                 <div key={index}
-                     className={`max-w-lg p-4 rounded-3xl my-1 ${message.isCurrentUser ? currentUserStyle : otherUserStyle}`}>
+                     className={`max-w-sm p-4 rounded-3xl my-1 ${message.isCurrentUser ? currentUserStyle : otherUserStyle}`}>
                     {message.message}
                 </div>
             ))}
