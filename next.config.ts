609ad28f31bcc60f87ebd7355619784fbd5b0848
timofei <teamheightline@mail.ru>
2024-12-23 16:48:14 +0300

    Сбросил кэш github pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {unoptimized: true},
   basePath: '/herzen-final-term-paper',
+  assetPrefix: '/herzen-final-term-paper/',
 };
 
 export default nextConfig;
