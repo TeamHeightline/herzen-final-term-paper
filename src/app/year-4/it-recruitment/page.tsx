bdb82d839b1d195047efe0b4b527034a819a2aa2
timofei <teamheightline@mail.ru>
2025-05-29 15:29:47 +0300

    Сделал страницу для IT рекрутмента

--- src/app/year-4/it-recruitment/page.tsx
+++ src/app/year-4/it-recruitment/page.tsx
@@ -1,7 +1,49 @@
 export default function ItRecruitment() {
   return (
-    <>
+    <div className={'w-full'}>
       <h1 className={'text-center'}>It рекрутмент</h1>
-    </>
+      <h2 id={'lr1'}>Лабораторная работа №1-2</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/it-recrutment/LR 1-2.pdf"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr2'}>Лабораторная работа №3</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/it-recrutment/LR 3.pdf"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr2'}>Лабораторная работа №4.1</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/it-recrutment/LR 4.1.png"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr2'}>Лабораторная работа №4.2</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/it-recrutment/LR 4.2.png"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr2'}>Лабораторная работа №5</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/it-recrutment/LR 5.pdf"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr2'}>Лабораторная работа №6-7</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/it-recrutment/LR 6-7.pdf"
+        width="100%"
+        height="700px"
+      />
+    </div>
   );
 }
