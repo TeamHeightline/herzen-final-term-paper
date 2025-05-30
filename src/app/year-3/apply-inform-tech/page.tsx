2c6b4fc878c0ce67daabf148a5d55f4c55d270e8
timofei <teamheightline@mail.ru>
2025-05-30 16:55:21 +0300

    Сделал страницу Прикладные информационные технологии

---
+++ src/app/year-3/apply-inform-tech/page.tsx
@@ -0,0 +1,21 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className={'text-center'}>Прикладные информационные технологии</h1>
+      <h2 id={'lr1'}>Задание №1</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-3/apply-inform-tech/T 1.pdf"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr1'}>Задание №2</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-3/apply-inform-tech/T 2.pdf"
+        width="100%"
+        height="700px"
+      />
+    </>
+  );
+}
