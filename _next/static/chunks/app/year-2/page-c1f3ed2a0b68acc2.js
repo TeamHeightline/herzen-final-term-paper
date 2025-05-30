e2661259e3c3893c67a5b06de74fdce2a6d1559c
timofei <teamheightline@mail.ru>
2025-05-30 18:35:21 +0300

    Updates

--- _next/static/chunks/app/year-2/page-c1f3ed2a0b68acc2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[446,731,857],{3170:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8173,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,441,517,358],()=>s(3170)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/year-4/page-f8cee5152cb2f109.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[324,446,731,857],{3170:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8173,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,441,517,358],()=>s(3170)),_N_E=e.O()}]);
