63129ca94f2910ed18d0bd506594725b661ad4ac
timofei <teamheightline@mail.ru>
2025-05-30 12:06:18 +0300

    Сделал страницу для предмета Мировые информационные ресурсы и цифровые библиотеки

---
+++ src/app/year-4/libraries/page.tsx
@@ -0,0 +1,77 @@
+export default function Libraries() {
+  return (
+    <>
+      <h1 className={'text-center'}>Мировые информационные ресурсы и цифровые библиотеки</h1>
+      <h2 id={'lr1'}>ИСР №1</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/libraries/ISR 1.pdf#toolbar=0"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr1'}>ВСР №2</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/libraries/VSR 2.pdf#toolbar=0"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr1'}>ВСР №3</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/libraries/VSR 3.pdf#toolbar=0"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr1'}>ВСР №5</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/libraries/VSR 5.pdf#toolbar=0"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr1'}>ВСР №6</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/libraries/VSR 6.pdf#toolbar=0"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr1'}>ВСР №7</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/libraries/VSR 7.pdf#toolbar=0"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr1'}>ВСР №8</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/libraries/VSR 8.pdf#toolbar=0"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr1'}>ВСР №9</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/libraries/VSR 9.pdf#toolbar=0"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr1'}>ВСР №10</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/libraries/VSR 10.pdf#toolbar=0"
+        width="100%"
+        height="700px"
+      />
+      <h2 id={'lr1'}>ВСР №11</h2>
+      <embed
+        className={'mt-4'}
+        src="/herzen-final-term-paper/files/year-4/libraries/VSR 11.pdf#toolbar=0"
+        width="100%"
+        height="700px"
+      />
+    </>
+  );
+}
